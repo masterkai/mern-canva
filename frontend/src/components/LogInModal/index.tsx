048d7e7641de9a338ce290ebd448742a4d78b403
maxLiu <neoliukai@gmail.com>
2024-05-24 11:55:51 +0800

    feat(login-modal): add form validation and input handling (main)

--- frontend/src/components/LogInModal/index.tsx
+++ frontend/src/components/LogInModal/index.tsx
@@ -1,4 +1,6 @@
 import { RxCross2 } from "react-icons/rx";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaFacebook } from "react-icons/fa";
 import { ChangeEvent } from "react";
 
 type LogInModalProps = {
@@ -22,6 +24,12 @@ const LogInModal = ({ show, setShow, inputHandle, state }: LogInModalProps) => {
                 <h2 className='text-white pb-4 text-center text-xl'>Login and Sign up in seconds</h2>
                 <form>
                     <div className='flex flex-col gap-3 mb-3 text-white'>
+                        <label htmlFor="name">Name</label>
+                        <input onChange={ inputHandle } type="name" name="name" id="name" placeholder='name'
+                               value={ state.name }
+                               className='px-3 py-2 rounded-md border outline-none border-[#5c5c5e] focus:border-purple-500 bg-transparent'/>
+                    </div>
+                    <div className='flex flex-col gap-3 mb-3 text-white'>
                         <label htmlFor="email">Email</label>
                         <input onChange={ inputHandle } type="email" name="email" id="email" placeholder='email'
                                value={ state.email }
@@ -33,6 +41,34 @@ const LogInModal = ({ show, setShow, inputHandle, state }: LogInModalProps) => {
                         <input onChange={ inputHandle } type="password" name="password" id="password"
                                placeholder='password' value={ state.password }
                                className='px-3 py-2 rounded-md border outline-none border-[#5c5c5e] focus:border-purple-500 bg-transparent'/>
+                    </div>
+
+                    <div>
+                        <button
+                            className='px-3 py-2 rounded-md bg-purple-500 w-full outline-none hover:bg-purple-600 text-white'>Singin
+                        </button>
+                    </div>
+
+                    <div className='flex py-4 justify-between items-center px-3'>
+                        <div className='w-[45%] h-[1px] bg-slate-500 '></div>
+                        <div className='w-[6%] text-center flex pb-1 px-1 text-white'>Or</div>
+                        <div className='w-[45%] h-[1px] bg-slate-500 '></div>
+                    </div>
+
+                    <div className='pb-4'>
+                        <button
+                            className='px-3 flex justify-center items-center gap-2 py-2 rounded-md bg-red-500 w-full outline-none hover:bg-red-600 text-white'>
+                            <span><BiLogoGmail/></span>
+                            <span>Login with Gmail </span>
+                        </button>
+                    </div>
+
+                    <div className='pb-4'>
+                        <button
+                            className='px-3 flex justify-center items-center gap-2 py-2 rounded-md bg-blue-500 w-full outline-none hover:bg-blue-600 text-white'>
+                            <span><FaFacebook/></span>
+                            <span>Login with Facebook </span>
+                        </button>
                     </div>
 
                 </form>
