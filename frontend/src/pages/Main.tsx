e1b90dc9d1bef4b7b5caf8e162e0e3c9e6a1237e
maxLiu <neoliukai@gmail.com>
2024-05-25 18:00:51 +0800

    Refactor: Replace horizontal side nav with vertical side nav
    This commit refactors the horizontal side navigation bar into a vertical one.
    
    The new vertical navigation bar is more compact and provides a better user experience on smaller screens. It also makes it easier to access the different sections of the application.

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -14,45 +14,7 @@ const Main = () => {
 			<Header_Design />
 
 			<div className='flex h-[calc(100%-60px)] w-screen'>
-				<div className='w-[80px] bg-[#18191B] z-50 h-full text-gray-400 overflow-y-auto'>
-
-					<div className={` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
-						<span className='text-2xl'><LuLayoutTemplate /></span>
-						<span className='text-xs font-medium'>Design</span>
-					</div>
-
-					<div className={` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
-						<span className='text-2xl'><FaShapes /></span>
-						<span className='text-xs font-medium'>Shapes</span>
-					</div>
-
-					<div className={` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
-						<span className='text-2xl'><FaCloudUploadAlt  /></span>
-						<span className='text-xs font-medium'>Upload</span>
-					</div>
-
-					<div className={` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
-						<span className='text-2xl'><FaTextHeight  /></span>
-						<span className='text-xs font-medium'>Text</span>
-					</div>
-
-
-					<div className={` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
-						<span className='text-2xl'><FaFolderOpen  /></span>
-						<span className='text-xs font-medium'>Project</span>
-					</div>
-
-					<div className={` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
-						<span className='text-2xl'><BsImages   /></span>
-						<span className='text-xs font-medium'>Images</span>
-					</div>
-
-					<div className={` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
-						<span className='text-2xl'><RxTransparencyGrid   /></span>
-						<span className='text-xs font-medium'>Background</span>
-					</div>
-
-				</div>
+				<SideNav />
 
 			</div>
 
@@ -62,3 +24,53 @@ const Main = () => {
 
 export default Main;
 
+const SideNav = () => {
+	return (
+		<div className='w-[80px] bg-[#18191B] z-50 h-full text-gray-400 overflow-y-auto'>
+
+			<div
+				className={ ` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100` }>
+				<span className='text-2xl'><LuLayoutTemplate/></span>
+				<span className='text-xs font-medium'>Design</span>
+			</div>
+
+			<div
+				className={ ` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100` }>
+				<span className='text-2xl'><FaShapes/></span>
+				<span className='text-xs font-medium'>Shapes</span>
+			</div>
+
+			<div
+				className={ ` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100` }>
+				<span className='text-2xl'><FaCloudUploadAlt/></span>
+				<span className='text-xs font-medium'>Upload</span>
+			</div>
+
+			<div
+				className={ ` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100` }>
+				<span className='text-2xl'><FaTextHeight/></span>
+				<span className='text-xs font-medium'>Text</span>
+			</div>
+
+
+			<div
+				className={ ` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100` }>
+				<span className='text-2xl'><FaFolderOpen/></span>
+				<span className='text-xs font-medium'>Project</span>
+			</div>
+
+			<div
+				className={ ` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100` }>
+				<span className='text-2xl'><BsImages/></span>
+				<span className='text-xs font-medium'>Images</span>
+			</div>
+
+			<div
+				className={ ` w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100` }>
+				<span className='text-2xl'><RxTransparencyGrid/></span>
+				<span className='text-xs font-medium'>Background</span>
+			</div>
+
+		</div>
+	)
+}
